8d833f7bdb931c395997c70e0e732e491541f283
kevinmala00 <64544191+kevinmala00@users.noreply.github.com>
2021-09-03 01:15:01 +0200

    add DestinazioniAdapter per la recycler view in PagDestinazioni

---
+++ app/src/main/java/com/example/ponyhelper/DestinazioniAdapter.java
@@ -0,0 +1,105 @@
+package com.example.ponyhelper;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ponyhelper.body.Destinazione;
+
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class DestinazioniAdapter extends RecyclerView.Adapter<DestinazioniAdapter.ViewHolder> {
+    private Context mContext;
+    private List<Destinazione> mlistDest;
+
+    public DestinazioniAdapter(List<Destinazione> listDest, Context context){
+        mContext=context;
+        mlistDest = listDest;
+    }
+
+
+    @NonNull
+    @Override
+    public DestinazioniAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View destView = inflater.inflate(R.layout.linea_consegne, parent, false);
+
+        // Return a new holder instance
+        return new ViewHolder(destView);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // Get the data model based on position
+        Destinazione destinazione = mlistDest.get(position);
+        //riempio i singoli elementi delle righe prelevando i dati dal turno
+        TextView tvIndirizzoCompleto = holder.tvIndirizzoCompleto;
+        String indirirzzo = destinazione.getIndirizzo().toStringFullAddress();
+        tvIndirizzoCompleto.setText(indirirzzo);
+
+        TextView tvDataOraUltimaMod= holder.tvDataOraUltimaMod;
+        tvDataOraUltimaMod.setText(destinazione.getDataUltimaModifica().format(DateTimeFormatter.ofPattern(("dd-MM-yyyy HH:mm"))));
+
+        TextView tvMancia= holder.tvMancia;
+        String mancia= destinazione.getMancia() + "€";
+        tvMancia.setText(mancia);
+
+        TextView tvNote= holder.tvNote;
+        tvNote.setText(destinazione.getNote());
+
+        ImageButton bPosition = holder.bPosition;
+        bPosition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String geoLocation = "geo:" + destinazione.getLatitudine() +"," + destinazione.getLongitudine() +"?q("+destinazione.getIndirizzo().toStringFullAddress()+")";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(geoLocation));
+                if (intent.resolveActivity(mContext.getPackageManager()) != null) {
+                    mContext.startActivity(intent);
+                }
+
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView tvIndirizzoCompleto;
+        public TextView tvDataOraUltimaMod;
+        public TextView tvMancia;
+        public TextView tvNote;
+        public ImageButton bPosition;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvIndirizzoCompleto=itemView.findViewById(R.id.tv_indirizzocompleto);
+            tvDataOraUltimaMod=itemView.findViewById(R.id.tv_dataultimamoddestinazione);
+            tvMancia=itemView.findViewById(R.id.tv_manciadestinazione);
+            tvNote=itemView.findViewById(R.id.tv_notedestinazione);
+            bPosition=itemView.findViewById(R.id.b_aggiunginDestinazione);
+
+
+        }
+    }
+}
