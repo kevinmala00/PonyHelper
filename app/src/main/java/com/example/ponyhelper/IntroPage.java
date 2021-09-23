65e4a6d129ddf353dd93e709ed81bd6fb8f12e93
kevinmala00 <64544191+kevinmala00@users.noreply.github.com>
2021-09-23 22:08:58 +0200

    aggiunta pagina introduttiva temporizzata

---
+++ app/src/main/java/com/example/ponyhelper/IntroPage.java
@@ -0,0 +1,26 @@
+package com.example.ponyhelper;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+
+public class IntroPage extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(IntroPage.this, HomePage.class);
+                startActivity(intent);
+            }
+        }, 2000);   //5 seconds
+
+
+    }
+}
